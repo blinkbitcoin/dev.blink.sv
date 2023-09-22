f6df7a9617c20322de97a5c9492d40c9b027f49c
openoms <oms@tuta.io>
2023-09-22 23:02:17 +0200

    feat: add analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,6 +57,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-QG87ZX5PS3',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
