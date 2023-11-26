69046e5903494db7c1921feedda2b3308f48c988
openoms <oms@tuta.io>
2023-11-26 17:02:13 +0100

    feat: add parsed data and explorer links to decode

--- src/components/DecodeInvoice.jsx
+++ src/components/DecodeInvoice.jsx
@@ -4,7 +4,8 @@ import useScript from '../hooks/useScript';
 
 export function DecodeInvoice() {
   const { paymentRequest, setPaymentRequest } = useAuth();
-  const [decodedInvoice, setDecodedInvoice] = useState(null);
+  const [rawData, setRawData] = useState(null);
+  const [decodedInvoice, setDecodedInvoice] = useState({}); //
   const [errorMessage, setErrorMessage] = useState(null);
 
   const status = useScript('/js/bolt11.min.js');
@@ -17,8 +18,10 @@ export function DecodeInvoice() {
 
   const handleDecode = () => {
     try {
+      let networkName;
       let customNetwork;
       if (paymentRequest.startsWith('lnbc')) {
+        networkName = "mainnet";
         customNetwork = {
           bech32: 'bc', // mainnet
           pubKeyHash: 0x42,
@@ -26,8 +29,17 @@ export function DecodeInvoice() {
           validWitnessVersions: [0, 2, 3, 4, 5]
         };
       } else if (paymentRequest.startsWith('lntbs')) {
+        networkName = "signet";
         customNetwork = {
           bech32: 'tbs', // signet
+          pubKeyHash: 0x42,
+          scriptHash: 0xff,
+          validWitnessVersions: [0, 2, 3, 4, 5]
+        };
+      } else if (paymentRequest.startsWith('lntb')) {
+        networkName = "testnet";
+        customNetwork = {
+          bech32: 'tb', // testnet
           pubKeyHash: 0x42,
           scriptHash: 0xff,
           validWitnessVersions: [0, 2, 3, 4, 5]
@@ -37,12 +49,52 @@ export function DecodeInvoice() {
       }
 
       const decoded = lightningPayReq.decode(paymentRequest, customNetwork); // Use the global object
-      setDecodedInvoice(decoded);
+      setRawData(decoded);
+
+      setDecodedInvoice({
+        network: networkName,
+        payeeNodeKey: decoded.payeeNodeKey,
+        satoshis: decoded.satoshis,
+        timestampString: new Date(decoded.timestamp * 1000).toISOString(),
+        link: generateLink(decoded.payeeNodeKey, networkName)
+      });
       setErrorMessage(null);
     } catch (error) {
-      setDecodedInvoice(null);
+      setDecodedInvoice({});
       setErrorMessage('Failed to decode invoice. Please check the format.');
     }
+  };
+
+  const generateLink = (payeeNodeKey, network) => {
+    switch (network) {
+      case 'mainnet':
+        return [
+          `https://amboss.space/node/${payeeNodeKey}`,
+          `https://mempool.space/lightning/node/${payeeNodeKey}`,
+          `https://1ml.com/node/${payeeNodeKey}`
+        ];
+      case 'testnet':
+        return [
+          `https://mempool.space/${network}/lightning/node/${payeeNodeKey}`,
+          `https://1ml.com/testnet/node/${payeeNodeKey}`
+        ];
+      case 'signet':
+        return [
+          `https://mempool.space/${network}/lightning/node/${payeeNodeKey}`,
+        ];
+      default:
+        return [];
+    }
+  };
+
+  const flexContainerStyle = {
+    display: 'flex',
+    marginBottom: '5px'
+  };
+
+  const labelStyle = {
+    minWidth: '130px',
+    fontWeight: 'bold'
   };
 
   return (
@@ -50,18 +102,53 @@ export function DecodeInvoice() {
       <textarea
         value={paymentRequest}
         onChange={(e) => setPaymentRequest(e.target.value)}
-        style={{ width: '100%', height: '5em' }} // Set height to 5 lines
+        style={{ width: '100%', height: '5em' }}
         placeholder="Paste a lightning invoice"
       />
-      <div style={{ marginTop: '10px' }}></div>
       <button onClick={handleDecode}>Decode</button>
 
       <div style={{ marginTop: '10px' }}>
-        {errorMessage && <div style={{ color: 'red' }}>Error: {errorMessage}</div>}
-        {decodedInvoice && (
+        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+        {rawData && (
           <div>
-            <strong>Decoded invoice:</strong>
-            <pre style={{ marginLeft: '10px', marginTop: '10px' }}>{JSON.stringify(decodedInvoice, null, 2)}</pre>
+            {decodedInvoice && (
+              <>
+                <strong>Decoded Invoice Data</strong><br />
+                <div style={{ marginTop: '10px' }}></div>
+                <div style={flexContainerStyle}>
+                  <div style={labelStyle}>network:</div>
+                  <div>{decodedInvoice.network}</div>
+                </div>
+                <div style={flexContainerStyle}>
+                  <div style={labelStyle}>amount (sats):</div>
+                  <div>{decodedInvoice.satoshis}</div>
+                </div>
+                <div style={flexContainerStyle}>
+                  <div style={labelStyle}>timestamp:</div>
+                  <div>{decodedInvoice.timestampString}</div>
+                </div>
+                <div style={flexContainerStyle}>
+                  <div style={labelStyle}>payee node key:</div>
+                  <div>{decodedInvoice.payeeNodeKey}</div>
+                </div>
+                <div style={flexContainerStyle}>
+                  <div style={labelStyle}>explore:</div>
+                </div>
+                {generateLink(decodedInvoice.payeeNodeKey, decodedInvoice.network).map((link, index) => (
+                  <div key={index}>
+                    <a href={link} target="_blank" rel="noopener noreferrer">{link}</a><br />
+                  </div>
+                ))}
+              </>
+            )}
+            <div>
+              <div style={{ marginTop: '20px' }}>
+                <strong>Raw Data</strong>
+                <pre style={{ marginLeft: '10px', marginTop: '10px' }}>
+                  {JSON.stringify(rawData, null, 2)}
+                </pre>
+              </div>
+            </div>
           </div>
         )}
       </div>
